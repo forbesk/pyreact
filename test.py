607293949c28b782e4948731ec34b4015dfa331c
Kevin Forbes <forbesk9@gmail.com>
2015-08-01 21:25:52 -0700

    Removed unused function

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from pyreact.Reactive import Reactive
 from pyreact.RxVar import RxVar
 from pyreact.RxFunc import RxFunc
 
+r = Reactive()
 x = RxVar(10)
 y = RxVar(20)
 sum = RxVar()
@@ -9,11 +10,6 @@ prod = RxVar()
 
 def sumxy():
     return x.get() + y.get()
-
-def prodxy():
-    return x.get() * y.get()
-    
-r = Reactive()
 
 r.connect(sum, [x, y], sumxy)
 r.connect(prod, [x, y], lambda : x.get() * y.get())
